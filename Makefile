c862ea0954864710bd6ab3e425d3f28271926aa6
Marcela Ribeiro de Oliveira <mro15@inf.ufpr.br>
2017-09-15 10:49:10 -0300

    gerador de subconjuntos pronto

--- Makefile
+++ Makefile
@@ -1,8 +1,7 @@
 CFLAGS=-Wall
 all: subconjuntos.o prox_subconjunto.o
-	gcc -o subconjuntos subconjuntos.o $(CFLAGS)
+	g++ -o subconjuntos subconjuntos.o $(CFLAGS)
 	gcc -o prox-subconj prox_subconjunto.o $(CFLAGS)
 
 clean:
 	rm *.o subconjuntos prox-subconj
-
---
+++ subconjuntos.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cstdlib>
+#include <stdio.h>
+#include <list>
+
+int cont = 0;
+int enumerate(int *set,  int n, int k, int offset, std::list<int> &subsets){
+    int i;
+    if(k==0){
+        cont++;
+        //for(std::list<int>::iterator it=subsets.begin(); it!=subsets.end() ; ++it)
+        //    std::cout << *it << " ";
+        return 0;
+    }
+    for(i=offset; i<n; ++i){
+        subsets.push_back(set[i]);
+        //puts("oi");
+        enumerate(set, n, k-1, i+1, subsets);
+        //puts("tchau");
+        subsets.pop_back();
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]){
+    if(argc!=3){
+        printf("Uso: <subconjuntos> <k> <n>\n");
+        return 0;
+    }
+    int *set, n, k, i;
+    n = atoi(argv[1]);
+    k = atoi(argv[2]);
+    //printf("n: %d, k: %d\n", n, k);
+    set = new int[n];
+    /*Create set [1...n]*/
+    for(i = 0; i<n; ++i){
+        set[i] = i+1;
+    }
+    /*Print set*
+    for(i = 0; i<n; ++i){
+        printf("%d ", set[i]);
+    }
+    /*Array of subsets has size k*/
+    std::list<int> subsets;
+    enumerate(set,  n, k, 0, subsets);
+    printf("%d\n", cont);
+    return 0;
+}
